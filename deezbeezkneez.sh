bf11295a7ae306313a32a1759a13fa95693a2d8b
omg <omg@orionsbelt.space>
2018-05-09 15:44:11 -0800

    new flag: -g | --gdrive Purpose is to pipe SMLoadr output direct to gdrive. **testing**

--- deezbeezkneez.sh
+++ deezbeezkneez.sh
@@ -8,21 +8,77 @@ LOGDATE="date +%Y%m%d%H%M%S"
 LOG="tee -a $OUTPUT/deezbeezknees$LOGDATE.log" # Path to logfile
 
 mir() {
-  rm -rf "$OUTPUT"/mirrordeezer.log # Removing existing mirrordeezer.log
-  x=13000000                        # Possible highest value of artists on Deezer
-  mkdir -p "$OUTPUT"                # Creates output folder if not already created
+  # rm -rf "$OUTPUT"/mirrordeezer.log # Removing existing mirrordeezer.log
+  mkdir -p "$OUTPUT" # Creates output folder if not already created
   $ECHO "Starting full mirror of Deezer starting at 1" | $LOG
   $ECHO "  		--quality parameter set to FLAC" | $LOG
   sleep 5
-  mirror=$(
-    seq $x | parallel -j4 --joblog --bar --eta \
-    "$SMLOADER -q 'FLAC' -d 'single' -p $OUTPUT \
-		"https://www.deezer.com/us/ar/us/artist/{}" "
-  )
-  $ECHO "$mirror" | $LOG
-  $ECHO "" | $LOG
-  $ECHO "" | $LOG
+
+  local x="13000000" # Possible highest value of artists on Deezer
+
+  local parallelARGS=("--link"
+    "-j12"
+    "--joblog=${parallel_log}"
+    "-X"
+    "--progress"
+    "--load=50%"
+    "--retries=3"
+    "--noswap"
+    "--memfree 128M"
+    "--resume-failed"
+    "--delay"
+    "--eta")
+
+  local smloaderARGS=("--quality='FLAC'"
+    "--downloadmode='single'"
+    "--path=$OUTPUT")
+
+  local smloaderSITE="https://www.deezer.com/us/ar/us/artist/"
+
+  seq $x | parallel "${parallelARGS[@]}" "$SMLOADER" "${smloaderARGS[@]}" ${smloaderSITE}{}
+  $ECHO "" | "$LOG"
+  $ECHO "" | "$LOG"
 }
+
+mirtocloud() {
+  # rm -rf "$OUTPUT"/mirrordeezer.log # Removing existing mirrordeezer.log
+  mkdir -p "$OUTPUT" # Creates output folder if not already created
+  $ECHO "Starting full mirror of Deezer starting at 1" | $LOG
+  $ECHO "  		--quality parameter set to FLAC" | $LOG
+  sleep 5
+
+  local x="13000000" # Possible highest value of artists on Deezer
+
+  local parallelARGS=("--link"
+    "-j12"
+    "--joblog=${parallel_log}"
+    "-X"
+    "--progress"
+    "--load=50%"
+    "--retries=3"
+    "--noswap"
+    "--memfree 128M"
+    "--resume-failed"
+    "--delay"
+    "--eta")
+
+  local smloaderARGS=("--quality='FLAC'"
+    "--downloadmode='single'"
+    "--path=$OUTPUT")
+
+  local smloaderSITE="https://www.deezer.com/us/ar/us/artist/"
+
+  local rcloneREMOTE="omg"
+  local rcloneOUTPUT="OMG_share/Plex-Media/deezbeezkneez/*"
+
+  local rcloneARGS="--checksum"
+
+  seq $x | parallel "${parallelARGS[@]}" "$SMLOADER" "${smloaderARGS[@]}" \
+  ${smloaderSITE}{} | rclone rcat $rcloneREMOTE:"$rcloneOUTPUT" "$rcloneARGS"
+  $ECHO "" | "$LOG"
+  $ECHO "" | "$LOG"
+}
+
 smloader() {
   # Removing existing mirrordeezer.log
   rm -rf "$OUTPUT"/mirrordeezer.log
@@ -47,6 +103,7 @@ hlp() {
   $ECHO ""
   $ECHO "Available options:"
   $ECHO "-m, --mirror		: Mirror deezer starting at artist 1"
+  $ECHO "-g, --gdrive		: Mirror deezer starting at artist 1 directly to gdrive"
   $ECHO "-h, --help		: This help page."
   $ECHO "-i, --install	: download SMloader to $HOME/bin"
   $ECHO "-r, --run		: run SMloader like normal. same as --config"
@@ -61,6 +118,10 @@ while [ ! $# -eq 0 ]; do
     ;;
   -m | --mirror)
     mir
+    exit 0
+    ;;
+  -g | --gdrive)
+    mirtocloud
     exit 0
     ;;
   -s | --smloader)
